b438e723a5ef2f18b8b5467f42cc967256bb86d7
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-04-25 13:16:21 +0100

    add: only print the env that contains =

--- _env.c
+++ _env.c
@@ -11,7 +11,15 @@ int _env(char **env, int args)
 	}
 
 	while (env[i])
-		printf("%s\n", env[i++]);
+	{
+		// check if '=' is present
+		if (strchr(env[i], '='))
+		{
+			// print the env variable
+			write(STDOUT_FILENO, env[i], _strlen(env[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+	}
 
 	return (EXIT_SUCCESS);
 }
