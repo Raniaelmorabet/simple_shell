f348aa15ab19dd798d5f5605d75b0d33546c1e12
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-04-25 21:27:33 -0700

    betty

--- _env.c
+++ _env.c
@@ -12,10 +12,10 @@ int _env(char **env, int args)
 
 	while (env[i])
 	{
-		// check if '=' is present
+		/* check if '=' is present */
 		if (strchr(env[i], '='))
 		{
-			// print the env variable
+			/* print the env variable */
 			write(STDOUT_FILENO, env[i], _strlen(env[i]));
 			write(STDOUT_FILENO, "\n", 1);
 		}
