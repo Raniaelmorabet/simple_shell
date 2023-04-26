7269ce70685887967bdafafac799f8f8e383a81e
“Raniaelmorabet” <raniaelmorabet9@gmail.com>
2023-04-26 00:50:08 -0700

    rm utoa

--- _itoa.c
+++ _itoa.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
 /**
  * get_number_length - gets the length of a number
  * @n: the number to get the length of
@@ -74,29 +72,3 @@ char *_itoa(ssize_t value, char *base)
 	return (reverse_string(result));
 }
 
-/**
- * _utoa - converts an unsigned integer to a string
- * @value: integer to convert
- * @base: base to convert to
- * Return: pointer to the converted string
- */
-char *_utoa(size_t value, char *base)
-{
-	ssize_t quotient = ABS(value), i = 0;
-	int base_len = _strlen(base);
-	char *result = malloc(get_number_length(value) + 1);
-
-	if (result == NULL)
-		return (NULL);
-
-	if (base_len < 2 || base_len > 16)
-		return (NULL);
-
-	do {
-		result[i++] = base[quotient % base_len];
-		quotient /= base_len;
-	} while (quotient);
-
-	result[i] = '\0';
-	return (reverse_string(result));
-}
